65e7e31b1ce0374e48cce96021d79fd07b2f5012
Adarsh Pati Tripathi <tripathi.adarshh05@gmail.com>
2026-04-23 01:05:42 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ const { errorHandler, notFoundHandler } = require('./middleware/errorMiddleware'
 const { ensureDatabase } = require('./models/db');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 2001;
 
 ensureDatabase();
 
